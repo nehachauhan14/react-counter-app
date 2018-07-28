949b6ad6c1533e5bc47d2f0835ac42ba7e499d85
Neha Chauhan <nehachauhan79@gmail.com>
2018-07-28 20:28:56 +0530

    first commit from system

---
+++ src/components/counter.jsx
@@ -0,0 +1,81 @@
+ import React, {Component} from 'react';
+
+ class Counter extends Component{
+
+    // constructor(){
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    //     this.handleDecrement = this.handleDecrement.bind(this);
+    //    console.log('In constructor' , this);
+    // }
+    state ={
+        value:this.props.value,
+        ImageUrl : "https://picsum.photos/100/50/?random",
+    };
+
+    styles={
+        fontsize : 10,
+        fontweight : "bold",
+        margin : "10px" 
+
+    }
+
+    renderTags() {
+        if(this.state.tags.length === 0) return <p>No tags are available!</p>;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>; 
+    }
+
+    handleIncrement = (product) => {
+        console.log(product.id);
+        this.setState({value : this.state.value+1})
+    }
+
+    handleDecrement = (product) => {
+        if(this.state.value > 0) {
+            this.setState({
+                value : this.state.value-1
+            })
+        }
+        
+    }
+
+    doHandleIncrement = ()=>{
+        this.handleIncrement({id:1});
+    }
+
+
+    render(){
+        console.log("props :" , this.props);
+        return (
+            
+            <div style={{margin:10}}>
+                 <h4>Title #{this.props.id}</h4>
+                <img src={this.state.ImageUrl} alt=""/>
+                <span style={this.styles} className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                    onClick={this.doHandleIncrement} 
+                    className="btn btn-secondary btn-sm m-2">Increment
+                </button>
+                <button
+                    onClick={this.props.onDelete}
+                    className="btn btn-danger btn-sm"> Decrement 
+                </button>
+            </div>
+        );
+    }
+
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += (this.state.value === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const {value} = this.state;
+        return value === 0 ? "Zero" :  value ;
+    }
+
+ }
+
+ export default Counter;
